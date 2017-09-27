195d2749d8b03646a8d628a6d14578b1a38039f5
Brandon Cuff <brandon@scopely.com>
2017-09-27 14:39:28 -0700

    Improved trace id collision

--- src/DataDog.Tracing/Util.cs
+++ src/DataDog.Tracing/Util.cs
@@ -8,13 +8,16 @@ namespace DataDog.Tracing
     internal static class Util
     {
         [ThreadStatic]
-        static Random _random;
+        static Random _random1;
+        [ThreadStatic]
+        static Random _random2;
 
-        static Random Random => _random ?? (_random = new Random(Guid.NewGuid().GetHashCode()));
+        static Random Random1 => _random1 ?? (_random1 = new Random(Guid.NewGuid().GetHashCode()));
+        static Random Random2 => _random2 ?? (_random2 = new Random(Guid.NewGuid().GetHashCode()));
 
-        public static long NewTraceId() => (long)(Random.Next() << 32) | (long)Random.Next();
+        public static long NewTraceId() => (long)(Random1.Next() << 32) | (long)Random2.Next();
 
-        public static long NewSpanId() => (long)(Random.Next() << 32) | (long)Random.Next();
+        public static long NewSpanId() => (long)(Random1.Next() << 32) | (long)Random2.Next();
 
         static readonly long EpochTicks = new DateTime(1970, 1, 1).Ticks;
 
