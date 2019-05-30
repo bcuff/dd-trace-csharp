f997fdc8f67eb73dfb03c3a4c50ca66110f4d17f
Brandon Cuff <brandon@scopely.com>
2019-05-30 15:39:57 -0700

    Release 1.0.6 - Support trace analytics

--- src/DataDog.Tracing.AspNetCore/DataDogTracingApplicationBuilderExtensions.cs
+++ src/DataDog.Tracing.AspNetCore/DataDogTracingApplicationBuilderExtensions.cs
@@ -7,13 +7,21 @@ namespace DataDog.Tracing.AspNetCore
 {
     public static class DataDogTracingApplicationBuilderExtensions
     {
-        public static IApplicationBuilder UseDataDogTracing(this IApplicationBuilder app, TraceSource source, string serviceName = "web") => app.Use(async (context, next) =>
+
+        public static IApplicationBuilder UseDataDogTracing(this IApplicationBuilder app, TraceSource source, string serviceName = "web")
+            => app.UseDataDogTracing(source, new TraceOptions { ServiceName = serviceName });
+
+        public static IApplicationBuilder UseDataDogTracing(this IApplicationBuilder app, TraceSource source, TraceOptions options) => app.Use(async (context, next) =>
         {
             var resource = context.Request.Host.Host;
             var path = context.Request.Path.HasValue ? context.Request.Path.Value : string.Empty;
-            using (var span = source.Begin("aspnet.request", serviceName, resource, "web"))
+            using (var span = source.Begin("aspnet.request", options.ServiceName, resource, "web"))
             using (var scope = new TraceContextScope(span))
             {
+                if (options.AnalyticsEnabled)
+                {
+                    span.SetMeta("_dd1.sr.eausr", "true");
+                }
                 span.SetMeta("http.method", context.Request.Method);
                 span.SetMeta("http.path", path);
                 span.SetMeta("http.query", context.Request.QueryString.HasValue ? context.Request.QueryString.Value : string.Empty);
---
+++ src/DataDog.Tracing.AspNetCore/TraceOptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataDog.Tracing.AspNetCore
+{
+    public class TraceOptions
+    {
+        public string ServiceName { get; set; } = "web";
+        public bool AnalyticsEnabled { get; set; }
+    }
+}
